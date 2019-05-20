632b2ab505762767b338bdfe01114b8bd9a08445
Myron <dubtwee@gmail.com>
2019-05-20 10:57:10 -0500

    creating an incrementer for the feeling state

--- src/components/FeelingPage/FeelingPage.js
+++ src/components/FeelingPage/FeelingPage.js
@@ -26,17 +26,28 @@ class FeelingPage extends Component {
         });
     }
 
+    clickDecrease = (event) => {
+        this.props.dispatch({
+            type: 'FEEDBACK_FEELING_DECREASE',
+        })
+    }
+
+    clickIncrease = (event) => {
+        this.props.dispatch({
+            type: 'FEEDBACK_FEELING_INCREASE',
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>How are you feeling today?</h2>
 
                 <div>
-                    <input
-                        type="number"
-                        placeholder="Feeling"
-                        onChange={this.changeField}
-                    />
+                    <button onClick={this.clickDecrease}>-</button>
+                    {this.props.reduxState.feeling}
+                    <button onClick={this.clickIncrease}>+</button>
+                    <br />
                     <button onClick={this.clickNext}>NEXT</button>
                 </div>
                 <div>
--- src/redux/reducers/feeling.reducer.js
+++ src/redux/reducers/feeling.reducer.js
@@ -1,6 +1,12 @@
-const feeling = (state = '', action) => {
-    if (action.type === 'FEEDBACK_FEELING') {
-        return action.payload;
+const feeling = (state = 0, action) => {
+    if (action.type === 'FEEDBACK_FEELING_DECREASE') {
+        const newstate = state - 1;
+        console.log('FEEDBACK_FEELING_DECREASE', newstate);
+        return newstate;
+    } else if (action.type === 'FEEDBACK_FEELING_INCREASE') {
+        const newstate = state + 1;
+        console.log('FEEDBACK_FEELING_INCREASE', newstate);
+        return newstate;
     }
     return state;
 }
