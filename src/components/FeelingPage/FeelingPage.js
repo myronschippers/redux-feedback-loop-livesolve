e533c882cd1524089aa945cb4f4b5e0fb6a16e43
Myron <dubtwee@gmail.com>
2019-05-19 22:53:43 -0500

    stubbing out the page content for the Feeling page of the feedback multi-step form

--- src/components/FeelingPage/FeelingPage.js
+++ src/components/FeelingPage/FeelingPage.js
@@ -4,7 +4,21 @@ import mapReduxStateToProps from '../../redux/mapReduxStateToProps';
 
 class FeelingPage extends Component {
     clickNext = (event) => {
+        this.props.dispatch({
+            type: 'FEEDBACK_FEELING',
+            payload: this.state.feeling,
+        });
         this.props.history.push('/understanding');
+    }
+
+    changeRating = (event) => {
+        this.setState({
+            feeling: event.target.value,
+        })
+    }
+
+    submitFeedback = (event) => {
+        console.log('Feedback has been submitted');
     }
 
     render() {
@@ -13,8 +27,22 @@ class FeelingPage extends Component {
                 <h2>How are you feeling today?</h2>
 
                 <div>
-                    PAGE CONTENT
+                    <input
+                        type="number"
+                        placeholder="Feeling"
+                        onChange={this.changeRating}
+                    />
                     <button onClick={this.clickNext}>NEXT</button>
+                </div>
+                <div>
+                    <h2>Review Feedback</h2>
+                    
+                    <p>Feelings: {this.props.reduxState.feelings}</p>
+                    <p>Understanding: {this.props.reduxState.understanding}</p>
+                    <p>Support: {this.props.reduxState.support}</p>
+                    <p>Comments: {this.props.reduxState.comments}</p>
+
+                    <button disabled={true} onClick={this.submitFeedback}>INCOMPLETE</button>
                 </div>
             </div>
         );
