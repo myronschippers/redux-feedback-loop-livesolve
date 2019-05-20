8d1f65147ca74e032b77e46225dd731891e3ee5b
Myron <dubtwee@gmail.com>
2019-05-19 21:56:33 -0500

    hooking up redux to the base feedback application

---
+++ src/redux/storeInstance.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+const feedbackReducer = (state = '', action) => {
+    return state;
+};
+
+const storeInstance = createStore(
+    combineReducers({
+        feedbackReducer,
+    }),
+    applyMiddleware(logger),
+);
+
+export default storeInstance;
