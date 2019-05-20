9dafdd0c4d8ea177c2a4691989c8d3ae764aab1b
Myron <dubtwee@gmail.com>
2019-05-20 09:15:58 -0500

    adding redux to application

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import storeInstance from './redux/storeInstance';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
