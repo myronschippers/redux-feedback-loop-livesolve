9f0d48ace078d52f6a6c48e851c847ec7a7495b5
Myron <dubtwee@gmail.com>
2020-08-27 13:07:52 -0500

    adjusting final settings to make container hosts communicate

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -1,20 +1,26 @@
-const express =  require('express');
+const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
 router.post('/', (req, res) => {
-    const feedbackData = req.body;
-    const dbQuery = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") 
+  const feedbackData = req.body;
+  const dbQuery = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") 
                     VALUES ($1, $2, $3, $4);`;
 
-    pool.query(dbQuery, [feedbackData.feeling, feedbackData.understanding, feedbackData.support, feedbackData.comments])
-        .then((response) => {
-            res.sendStatus(201);
-        })
-        .catch((err) => {
-            console.log('Error on post: ', err);
-            res.sendStatus(500);
-        });
+  pool
+    .query(dbQuery, [
+      feedbackData.feeling,
+      feedbackData.understanding,
+      feedbackData.support,
+      feedbackData.comments,
+    ])
+    .then((response) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log('Error on post: ', err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
